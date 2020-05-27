c5d68dc287ea715f19a5af8e223a4c065a1f74b8
Tom Badaczewski <tombadaczewski@gmail.com>
2020-05-27 16:15:42 -0500

    web form config prep

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,2 @@
 FLASK_APP=trivia.py
+PYTHONPATH=trivia
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Hello, World!"
+    user = {'username': 'Tom'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Ready for some trivia!'
+        },
+        {
+            'author': {'username': 'Bob'},
+            'body': 'I know lots of dumb things!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
