14b6fad80c1d584b8a06b08edc966e8223c5d186
Tom Badaczewski <tombadaczewski@gmail.com>
2020-06-05 18:47:22 -0500

    db prep, login prep

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,19 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import routes
+# Initialize Database
+db = SQLAlchemy(app)
+
+# Initialize migration engine
+migrate = Migrate(app, db)
+
+login = LoginManager(app)
+
+from app import routes, models
